8b29c24b79b967a9051015c3432dc18f2f8e97ee
whuang001 <vincentwongad@gmail.com>
2017-11-05 15:14:48 -0500

    change code style

--- test/unit /GateWayTest.py
+++ test/unit /GateWayTest.py
@@ -7,9 +7,9 @@ from testAndPrint import *
 def main():
 
 	Manager = GateWay()
-
+	#when no found
 	testAndPrint(Manager.vaild_user('if'), False)
-
+	#when found
 	testAndPrint(Manager.vaild_user('testuser2'), True)
 
 main()
--- test/unit /testAndPrint.py
+++ test/unit /testAndPrint.py
@@ -4,11 +4,9 @@ def testAndPrint(got, expected, task_name = None):
 
 	try:
 		if got != expected:
-
 			if task_name is None:
 				print('Got {}, expected {}'.format(got, expected))
 			else:
 				print('{}:\nGot {}, expected {}'.format(task_name, got, expected))
-
 	except Exception as e:
 			traceback.print_exc(e)
