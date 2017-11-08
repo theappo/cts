df255de7f5ef8362ceed1bd7c81cf7028940b704
whuang001 <vincentwongad@gmail.com>
2017-11-07 19:57:29 -0500

    add search_blacklist

--- src/StoredProcedure.py
+++ src/StoredProcedure.py
@@ -1,2 +1,5 @@
-# vaild_user_id: retrun user_id if exit in users table
-vaild_user_id = "SELECT user_id FROM users WHERE user_id = %s"
+# search user_id in blacklist table
+search_blacklist = "SELECT * FROM blacklist WHERE user_id = %s"
+
+# search user_id and user_password in user table
+verify_user = "SELECT * FROM users WHERE user_id = %s AND password = %s"
