ddbeabe50475de7d5b6d5dc5be47ac9985fcc5aa
qzsama <kelvinchao1996@gmail.com>
2017-11-25 21:53:32 -0500

    class fixed

--- src/Application.py
+++ src/Application.py
@@ -1,18 +1,15 @@
 class Application():
 	
-	def __init__(self, user_id,user_type,deposit_amount):
-		self._user_type = user_type
-		self._user_id = user_id
-		self._deposit_amount=deposit_amount
-
-	def get_user_id(self):
-		return self._user_id
-
-	def get_user_type(self):
-		return self._user_type
-
-	def get_deposit_amount(self):
-		return self._deposit_amount
+	def __init__(self,user_id,user_password,balance,user_type,email,address,deposit_amount):
+		self.user_id = user_id
+		self.user_password=user_password
+		self.balance=balance
+		self.user_type = user_type
+		self.email = email
+		self.address = address
+		self.deposit_amount = deposit_amount
+		
+	
 		
 # a=Application(1,1,1)
 # print(a.get_user_id(),a.get_user_type(),a.get_deposit_amount())
