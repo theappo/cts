30b0de82b282408080896cd92c4140374e5731b9
suman <sumanislambd@gmail.com>
2017-11-27 01:36:02 -0500

    message.py

---
+++ src/message.py
@@ -0,0 +1,8 @@
+class Message():
+    def __init__(self, message_id,sender,receiver,time):
+
+		self.message_id=message_id
+		self.sender=sender
+		self.receiver=receiver
+        self.time= time
+        
