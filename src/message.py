80be4ef8076792d4bad72753872f4852a104ce79
suman <sumanislambd@gmail.com>
2017-12-01 13:28:30 -0500

    delete message

--- src/message.py
+++
@@ -1,8 +0,0 @@
-class Message():
-    def __init__(self, message_id,sender,receiver,time):
-
-		self.message_id=message_id
-		self.sender=sender
-		self.receiver=receiver
-                self.time= time
-        
